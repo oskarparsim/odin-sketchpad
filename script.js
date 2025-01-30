8388c0c8db7ea05a3c9996a0a522aac2beab7072
Oskar <79008610+oskarparsim@users.noreply.github.com>
2025-01-30 19:01:13 +0200

    Added random colors per square and increasing opacity

--- script.js
+++ script.js
@@ -23,17 +23,31 @@ function deleteGrid() {
 
 drawGrid(gridSize);
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 squareContainer.addEventListener('mouseover', (e) => {
     if (e.target.className === 'square') {
-        e.target.style.backgroundColor = "black";
-    }
+        if (e.target.style.backgroundColor === ""){
+            let r = getRandomInt(0, 255);
+            let g = getRandomInt(0, 255);
+            let b = getRandomInt(0, 255);
+            e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+            e.target.style.opacity = "0.1";
+        } else {
+            e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.1;
+        }
+    };
 })
 
-squareContainer.addEventListener('mouseout', (e) => {
-    if (e.target.className === "square") {
-        e.target.style.backgroundColor = "";
-    }
-})
+// squareContainer.addEventListener('mouseout', (e) => {
+//     if (e.target.className === "square") {
+//         e.target.style.backgroundColor = "";
+//     }
+// })
 
 const btn = document.getElementById("gridSizeBtn");
 btn.addEventListener('click', (e) => {
