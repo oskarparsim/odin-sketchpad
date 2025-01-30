b53195bcdc4a52f427cf36b862363503bee2a8db
Oskar <79008610+oskarparsim@users.noreply.github.com>
2025-01-30 04:11:38 +0200

    Added 'hover' effect, so that the square turns black

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ const squareContainer = document.querySelector("#squareContainer");
 
 for (let i = 1; i <= 16; i++) {
     const row = document.createElement("div");
-    row.className = `row${i}`
+    row.className = `column`
+    row.id = `col${i}`
     squareContainer.appendChild(row);
     for (let j = 1; j <= 16; j++) {
         const div = document.createElement("div");
-        div.className = "square";
+        let count = ((i-1) * 16) + j;
+        div.className = `square`;
+        div.id = `sqr${count}`;
         row.appendChild(div);
     }
 }
+
+squareContainer.addEventListener('mouseover', (e) => {
+    if (e.target.className === 'square') {
+        e.target.style.backgroundColor = "black";
+    }
+})
+
+squareContainer.addEventListener('mouseout', (e) => {
+    if (e.target.className === "square") {
+        e.target.style.backgroundColor = "";
+    }
+})
